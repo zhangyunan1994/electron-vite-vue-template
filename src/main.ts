3e77ed5b9223e86653075e84e44b923c350499d5
yunan.zhang <zyndev@gmail.com>
2023-10-04 14:40:49 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import naive from 'naive-ui'
+import './assets/css/tailwindmain.css'
+
+import { setupAssets, setupScrollbarStyle } from './plugins'
+import { setupRouter } from './router'
+import { setupStore } from "./store"
+
+async function bootstrap() {
+  const app = createApp(App)
+  setupAssets()
+
+  setupScrollbarStyle()
+  setupStore(app)
+  await setupRouter(app)
+  app.use(naive)
+  app.mount('#app')
+}
+
+bootstrap()
+
